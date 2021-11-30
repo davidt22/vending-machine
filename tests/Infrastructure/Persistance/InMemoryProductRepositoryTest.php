5ea0e2b29a39a1bd9562762f78966a9c0fab8f34
David Teruel <d_teruel@hotmail.com>
2021-11-30 18:57:34 +0100

    Add test

--- tests/Infrastructure/Persistance/InMemoryProductRepositoryTest.php
+++ tests/Infrastructure/Persistance/InMemoryProductRepositoryTest.php
@@ -2,45 +2,53 @@
 
 namespace App\Tests\Infrastructure\Persistance;
 
+use App\Application\VendingMachine\CreateVendingMachine\CreateVendingMachineRequest;
+use App\Application\VendingMachine\CreateVendingMachine\CreateVendingMachineService;
 use App\Domain\Model\Product\Product;
 use App\Domain\Model\Product\ProductId;
 use App\Domain\Model\Product\ProductName;
 use App\Domain\Model\Product\ProductPrice;
 use App\Domain\Model\Product\ProductQuantity;
-use App\Domain\Model\VendingMachine\VendingMachine;
 use App\Infrastructure\Persistance\Product\InMemoryProductRepository;
+use App\Infrastructure\Persistance\VendingMachine\InMemoryVendingMachineRepository;
 use PHPUnit\Framework\TestCase;
 
 class InMemoryProductRepositoryTest extends TestCase
 {
-    private InMemoryProductRepository $productRepository;
+    private CreateVendingMachineService $createVendingMachineService;
+    private InMemoryProductRepository $inMemoryProductRepository;
+
     protected function setUp():void
     {
         parent::setUp();
 
-        $this->productRepository = new InMemoryProductRepository();
+        $this->inMemoryProductRepository = new InMemoryProductRepository();
+        $inMemoryVendingMachineRepository = new InMemoryVendingMachineRepository();
+        $this->createVendingMachineService = new CreateVendingMachineService($inMemoryVendingMachineRepository);
     }
 
     public function testItGeneratesNewIdentity()
     {
-        $id = $this->productRepository->nextIdentity();
+        $id = $this->inMemoryProductRepository->nextIdentity();
 
         $this->assertEquals(ProductId::class, get_class($id));
     }
 
-//    public function testItAddsNewProduct()
-//    {
-//        $vendingMachine = $this->createMock(VendingMachine::class);
-//        $product = Product::create(
-//            new ProductId(),
-//            ProductName::fromValue(ProductName::SODA),
-//            ProductPrice::fromValue(0.1),
-//            ProductQuantity::fromValue(10),
-//            $vendingMachine
-//        );
-//
-//        $this->productRepository->add($product, $vendingMachine);
-//
-//        $this->assertCount(1, $vendingMachine->products());
-//    }
+    public function testItAddsNewProduct()
+    {
+        $request = new CreateVendingMachineRequest('machine', []);
+        $vendingMachine = $this->createVendingMachineService->execute($request);
+
+        $product = Product::create(
+            new ProductId(),
+            ProductName::fromValue(ProductName::SODA),
+            ProductPrice::fromValue(0.1),
+            ProductQuantity::fromValue(10),
+            $vendingMachine
+        );
+
+        $vendingMachine->addProduct($product);
+
+        $this->assertCount(1, $vendingMachine->products());
+    }
 }
